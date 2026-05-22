60d7caa84289d6ec1f608e17054087e48206dd2f
Shae <dev.shailendra43@gmail.com>
2026-05-22 18:12:35 +0530

    chore: update debug baseUrl with active ngrok tunnel URL

--- Mobileapp/lib/constants/api_config.dart
+++ Mobileapp/lib/constants/api_config.dart
@@ -10,9 +10,9 @@ class ApiConfig {
 
   /// Base URL dynamically switching between local backend (in debug) and production.
   static String get baseUrl {
-    // To test with a local backend on your PC, uncomment the line below:
-    // if (kDebugMode) return 'http://192.168.29.24:5000';
-
+    if (kDebugMode) {
+      return 'https://75f0-2405-201-402a-504a-10e8-fe88-3839-255c.ngrok-free.app';
+    }
     return 'https://appapi.swastikjewel.in';
   }
 }
